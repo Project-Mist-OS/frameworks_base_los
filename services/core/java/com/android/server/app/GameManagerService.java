09a5dd0ff66389cbef021ba340d81bbc98f1bf46
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-01 08:22:50 +0800

    GameManagerService: release memory when switching to game
    Change-Id: Ifbe58ec13ed0eb3dc79979deb9bbfc34a299939e
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/app/GameManagerService.java
+++ services/core/java/com/android/server/app/GameManagerService.java
@@ -2339,6 +2339,7 @@ public final class GameManagerService extends IGameManagerService.Stub {
                         || mNonGameForegroundUids.isEmpty())) {
                     Slog.v(TAG, "Game power mode ON (first game in foreground)");
                     mPowerManagerInternal.setPowerMode(Mode.GAME, true);
+                    releaseMemory();
                 }
                 final boolean isGameDefaultFrameRateDisabled =
                         mSysProps.getBoolean(
@@ -2364,6 +2365,7 @@ public final class GameManagerService extends IGameManagerService.Stub {
                     if (mNonGameForegroundUids.isEmpty() && !mGameForegroundUids.isEmpty()) {
                         Slog.v(TAG, "Game power mode ON (only games in foreground)");
                         mPowerManagerInternal.setPowerMode(Mode.GAME, true);
+                        releaseMemory();
                     }
                 }
             }
@@ -2406,6 +2408,13 @@ public final class GameManagerService extends IGameManagerService.Stub {
                 }
             }
             return packagePidMap;
+        }
+        
+        private void releaseMemory() {
+            try {
+                ActivityManager.getService().releaseMemory(900, 40, false, false);
+            } catch (RemoteException e) {
+            }
         }
     }
 
