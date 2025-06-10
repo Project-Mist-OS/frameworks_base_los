d8003386c7fb7ea1f1d55b38fb2d8b52340130f8
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-11 06:13:14 +0800

    KeyProviderManager: add support for external keybox
    Change-Id: I3b25d793db335103e1b33e5661f08f15bde0361c
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- core/java/com/android/internal/util/custom/KeyProviderManager.java
+++ core/java/com/android/internal/util/custom/KeyProviderManager.java
@@ -7,9 +7,14 @@ package com.android.internal.util.custom;
 import android.app.ActivityThread;
 import android.content.Context;
 import android.util.Log;
+import android.os.SystemProperties;
 
 import com.android.internal.R;
 
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+
+import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,11 +25,7 @@ import java.util.Map;
  */
 public final class KeyProviderManager {
     private static final String TAG = "KeyProviderManager";
-
     private static final IKeyboxProvider PROVIDER = new DefaultKeyboxProvider();
-
-    private KeyProviderManager() {
-    }
 
     public static IKeyboxProvider getProvider() {
         return PROVIDER;
@@ -34,34 +35,102 @@ public final class KeyProviderManager {
         return PROVIDER.hasKeybox();
     }
 
+    private static void dlog(String msg) {
+        if (SystemProperties.getBoolean("persist.sys.keybox_debug", false)) {
+            Log.d(TAG, msg);
+        }
+    }
+
     private static class DefaultKeyboxProvider implements IKeyboxProvider {
         private final Map<String, String> keyboxData = new HashMap<>();
 
         private DefaultKeyboxProvider() {
-            Context context = getApplicationContext();
-            if (context == null) {
-                Log.e(TAG, "Failed to get application context");
-                return;
-            }
+            try {
+                File xmlFile = new File("/data/misc/keybox/keybox.xml");
+                if (!xmlFile.exists()) {
+                    dlog("Keybox XML file not found: " + xmlFile.getAbsolutePath());
+                    return;
+                }
 
-            String[] keybox = context.getResources().getStringArray(R.array.config_certifiedKeybox);
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(xmlFile);
+                doc.getDocumentElement().normalize();
 
-            Arrays.stream(keybox)
-                    .map(entry -> entry.split(":", 2))
-                    .filter(parts -> parts.length == 2)
-                    .forEach(parts -> keyboxData.put(parts[0], parts[1]));
+                NodeList keyboxes = doc.getElementsByTagName("Keybox");
 
-            if (!hasKeybox()) {
-                Log.w(TAG, "Incomplete keybox data loaded");
+                for (int i = 0; i < keyboxes.getLength(); i++) {
+                    Element keyboxElement = (Element) keyboxes.item(i);
+                    NodeList keys = keyboxElement.getElementsByTagName("Key");
+
+                    for (int j = 0; j < keys.getLength(); j++) {
+                        Element keyElement = (Element) keys.item(j);
+                        String algorithm = keyElement.getAttribute("algorithm").toUpperCase();
+                        if (algorithm.equals("ECDSA")) algorithm = "EC";
+
+                        Element privKeyElem = (Element) keyElement.getElementsByTagName("PrivateKey").item(0);
+                        String privKeyRaw = getRawText(privKeyElem);
+                        String privKey = extractBase64FromPEM(privKeyRaw);
+                        keyboxData.put(algorithm + ".PRIV", privKey);
+
+                        NodeList certList = keyElement.getElementsByTagName("Certificate");
+                        for (int k = 0; k < certList.getLength(); k++) {
+                            Element certElem = (Element) certList.item(k);
+                            String certRaw = getRawText(certElem);
+                            String cert = extractBase64FromPEM(certRaw);
+                            keyboxData.put(algorithm + ".CERT_" + (k + 1), cert);
+                        }
+                    }
+                }
+
+                if (!hasKeybox()) {
+                    dlog("Incomplete keybox data loaded");
+                    logMissingKeys();
+                } else {
+                    logLoadedKeys();
+                }
+            } catch (Exception e) {
+                dlog("Error reading keybox XML: " + e.getMessage());
             }
         }
 
-        private static Context getApplicationContext() {
-            try {
-                return ActivityThread.currentApplication().getApplicationContext();
-            } catch (Exception e) {
-                Log.e(TAG, "Error getting application context", e);
-                return null;
+        private String extractBase64FromPEM(String pem) {
+            return pem.replaceAll("-----BEGIN [^-]+-----", "")
+                      .replaceAll("-----END [^-]+-----", "")
+                      .replaceAll("[\\r\\n\\s]+", "");
+        }
+
+        private String getRawText(Element element) {
+            StringBuilder builder = new StringBuilder();
+            NodeList children = element.getChildNodes();
+            for (int i = 0; i < children.getLength(); i++) {
+                Node node = children.item(i);
+                if (node.getNodeType() == Node.TEXT_NODE || node.getNodeType() == Node.CDATA_SECTION_NODE) {
+                    builder.append(node.getNodeValue());
+                }
+            }
+            return builder.toString().trim();
+        }
+
+        private void logLoadedKeys() {
+            dlog("Successfully loaded keybox data:");
+            for (String key : Arrays.asList(
+                    "EC.PRIV", "EC.CERT_1", "EC.CERT_2", "EC.CERT_3",
+                    "RSA.PRIV", "RSA.CERT_1", "RSA.CERT_2", "RSA.CERT_3")) {
+                String value = keyboxData.get(key);
+                if (value != null) {
+                    dlog(key + ": " + value);
+                }
+            }
+        }
+
+        private void logMissingKeys() {
+            for (String key : Arrays.asList(
+                    "EC.PRIV", "EC.CERT_1", "EC.CERT_2", "EC.CERT_3",
+                    "RSA.PRIV", "RSA.CERT_1", "RSA.CERT_2", "RSA.CERT_3")) {
+                if (!keyboxData.containsKey(key)) {
+                    dlog("Missing key: " + key);
+                }
             }
         }
 
