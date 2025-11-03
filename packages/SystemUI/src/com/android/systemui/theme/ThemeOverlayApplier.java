d0d18240b963d6ea3a9e90be49d7f4923947a9bb
minaripenguin <minaripenguin@users.noreply.github.com>
2025-11-03 17:49:40 +0000

    Add support for power menu styles Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -138,6 +138,9 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_NOTIFICATION =
             "android.theme.customization.notification";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_POWER_MENU =
+            "android.theme.customization.powermenu";
 
     /*
      * All theme customization categories used by the system, in order that they should be applied,
@@ -156,7 +159,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SIGNAL,
             OVERLAY_CATEGORY_ICON_WIFI,
             OVERLAY_CATEGORY_PROGRESS_BAR,
-            OVERLAY_CATEGORY_NOTIFICATION);
+            OVERLAY_CATEGORY_NOTIFICATION,
+            OVERLAY_CATEGORY_POWER_MENU);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -172,7 +176,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SIGNAL,
             OVERLAY_CATEGORY_ICON_WIFI,
             OVERLAY_CATEGORY_PROGRESS_BAR,
-            OVERLAY_CATEGORY_NOTIFICATION);
+            OVERLAY_CATEGORY_NOTIFICATION,
+            OVERLAY_CATEGORY_POWER_MENU);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -220,6 +225,7 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_WIFI, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_PROGRESS_BAR, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NOTIFICATION, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_POWER_MENU, SYSUI_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }
