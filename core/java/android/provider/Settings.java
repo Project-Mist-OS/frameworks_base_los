c4b99853255c90bab87fca081b932499b5fa5231
Aston-Martinn <advaithbhat9@gmail.com>
2025-11-03 20:18:00 +0000

    SystemUI: Custom statusbar logo color options [1/2] Change-Id: Ieab255ebe1e9817360967e0f354342e716d2ae7f Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6861,6 +6861,18 @@ public final class Settings {
         public static final String STATUS_BAR_LOGO_STYLE = "status_bar_logo_style";
 
         /**
+         * Statusbar logo custom color
+         * @hide
+         */
+        public static final String STATUS_BAR_LOGO_COLOR = "status_bar_logo_color";
+
+        /**
+         * Statusbar logo custom color picker
+         * @hide
+         */
+        public static final String STATUS_BAR_LOGO_COLOR_PICKER = "status_bar_logo_color_picker";
+
+        /**
          * Network traffic indicator
          * 0 = Disabled
          * 1 = Enabled
