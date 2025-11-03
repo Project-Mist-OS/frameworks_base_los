9436e7a077ddb96d1a0fbb196fe77d728b65c11d
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-03 07:53:59 +0000

    [FEATURE]: add nothingOS always-on-display changes reverse-engineered from nothingOS 3.0
    Change-Id: Iab6bd5f7a6169aacec914a7e423e27f2ebaccd70
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -827,6 +827,7 @@ public final class NotificationPanelViewController implements
             @Override
             public boolean onDoubleTap(MotionEvent e) {
                 if (mPowerManager != null) {
+                    com.android.systemui.util.TapPositionUtil.INSTANCE().setTapPos((int) e.getX(), (int) e.getY());
                     mPowerManager.goToSleep(e.getEventTime());
                 }
                 return true;
@@ -4556,5 +4557,14 @@ public final class NotificationPanelViewController implements
         } else {
             mQsHeaderImageView.setImageDrawable(dw);
         }
+    }
+
+    public boolean isPanelFullyCollapsed() {
+        int state = mBarState;
+        if (state == StatusBarState.SHADE_LOCKED 
+            || state == StatusBarState.KEYGUARD) {
+            return !mQsController.isVisible();
+        }
+        return mExpandedFraction <= 0.0f;
     }
 }
