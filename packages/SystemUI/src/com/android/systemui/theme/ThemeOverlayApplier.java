4a75c35df9d406e3b902566f3075dcdca9563373
Zabuka_zuzu <zuzu950444@gmail.com>
2025-11-04 18:12:44 +0000

    base: revert power menu styles & notification themes progress bar styles

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -132,16 +132,6 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_LOCK_CLOCK_FONT =
             "android.theme.customization.lockscreen_clock_font";
-    @VisibleForTesting
-    static final String OVERLAY_CATEGORY_PROGRESS_BAR =
-            "android.theme.customization.progress_bar";
-    @VisibleForTesting
-    static final String OVERLAY_CATEGORY_NOTIFICATION =
-            "android.theme.customization.notification";
-    @VisibleForTesting
-    static final String OVERLAY_CATEGORY_POWER_MENU =
-            "android.theme.customization.powermenu";
-
     /*
      * All theme customization categories used by the system, in order that they should be applied,
      * starts with launcher and grouped by target package.
@@ -157,10 +147,7 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_ICON_SETTINGS,
             OVERLAY_CATEGORY_ICON_SIGNAL,
-            OVERLAY_CATEGORY_ICON_WIFI,
-            OVERLAY_CATEGORY_PROGRESS_BAR,
-            OVERLAY_CATEGORY_NOTIFICATION,
-            OVERLAY_CATEGORY_POWER_MENU);
+            OVERLAY_CATEGORY_ICON_WIFI);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -174,10 +161,7 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_ICON_SIGNAL,
-            OVERLAY_CATEGORY_ICON_WIFI,
-            OVERLAY_CATEGORY_PROGRESS_BAR,
-            OVERLAY_CATEGORY_NOTIFICATION,
-            OVERLAY_CATEGORY_POWER_MENU);
+            OVERLAY_CATEGORY_ICON_WIFI);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -223,9 +207,6 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SETTINGS, SETTINGS_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SIGNAL, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_WIFI, SYSUI_PACKAGE);
-        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_PROGRESS_BAR, ANDROID_PACKAGE);
-        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NOTIFICATION, SYSUI_PACKAGE);
-        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_POWER_MENU, SYSUI_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }
